3065eef016649e5b08906e86c29e0e3e5acd68ba
palakbhawsar5 <94602436+palakbhawsar5@users.noreply.github.com>
2023-02-27 01:26:08 +0530

    Add files via upload

---
+++ proj_3/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("Hey!!!")
+st.title(":green[I am PALAK BHAWSAR]")
+
+
+btn_click = st.button(" can connect with me: ")
+
+if btn_click == True:
+    st.subheader("yay:heart_eyes:")
+    st.text("here's my github account link:")
+    st.text("https://github.com/palakbhawsar5")
+    
+    st.text("here's the linkedIn account link:")
+    st.text("http://www.linkedin.com/in/palak-bhawsar-12b24120a")
+    st.balloons()
+
+st.subheader(":violet[Find it informative with my datasets about coffee and tea.]")    
